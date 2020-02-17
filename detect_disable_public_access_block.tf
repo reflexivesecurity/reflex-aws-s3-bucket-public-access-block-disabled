130647cb6877dbb57c2f3cde894a5127510e6da3
Richard Julian <richard@rjulian.net>
2020-02-17 11:35:19 -0800

    Patch: Update SNS_TOPIC variable

--- detect_disable_public_access_block.tf
+++ detect_disable_public_access_block.tf
@@ -31,7 +31,7 @@ PATTERN
   source_code_dir          = "${path.module}/source"
   handler                  = "public_access_block.lambda_handler"
   lambda_runtime           = "python3.7"
-  environment_variable_map = { SNS_TOPIC = "DetectDisablePublicAccessBlock" }
+  environment_variable_map = { SNS_TOPIC = module.detect_disable_public_access_block.sns_topic_arn }
   custom_lambda_policy     = <<EOF
 {
   "Version": "2012-10-17",
