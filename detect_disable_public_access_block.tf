aca4240a4082192802e79b2eea4f0512c520c116
rjulian <richard@rjulian.net>
2020-02-17 14:22:04 -0800

    Patch: pin version of engine

--- detect_disable_public_access_block.tf
+++ detect_disable_public_access_block.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "detect_disable_public_access_block" {
-  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_lambda"
+  source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_lambda?ref=v0.0.1"
   rule_name        = "DetectDisablePublicAccessBlock"
   rule_description = "Rule to detect a change in public access block configuration"
 
