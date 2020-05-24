a197878d8b132e89b0ec7061e64537bb1ac94221
Max Anderson <maxwell.c.anderson@gmail.com>
2020-05-23 17:52:49 -0700

    Fix terraform values

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -3,19 +3,19 @@ module "sqs_lambda" {
 
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
-  function_name             = "CloudtrailDeleted"
+  function_name             = "S3BucketPublicAccessBlockDisabled"
   source_code_dir           = "${path.module}/../../source"
-  handler                   = "reflex_aws_cloudtrail_deleted.lambda_handler"
+  handler                   = "s3_bucket_public_access_block_disabled.lambda_handler"
   lambda_runtime            = "python3.7"
   environment_variable_map = {
     SNS_TOPIC = var.sns_topic_arn,
 
   }
 
-  queue_name    = "CloudtrailDeleted"
+  queue_name    = "S3BucketPublicAccessBlockDisabled"
   delay_seconds = 0
 
-  target_id = "CloudtrailDeleted"
+  target_id = "S3BucketPublicAccessBlockDisabled"
 
   sns_topic_arn  = var.sns_topic_arn
   sqs_kms_key_id = var.reflex_kms_key_id
